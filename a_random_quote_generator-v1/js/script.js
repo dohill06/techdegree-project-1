303b5a171d3a35d0dc18eb9657126aa7921f87b8
Douglas Hill <dohill06@gmail.com>
2019-01-15 17:52:19 -0800

    Cleaned up notes.

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -3,20 +3,12 @@ Treehouse Techdegree:
 FSJS project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+/* 
+  Created an array of quote objects and named it quotes.
+  Added an extra occupation property to some objects for extra credit.
+ */
 
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-
-  Recommended: 
-    - Add at least one `year` and/or `citation` property to at least one 
-      quote object.
-***/
-
-// Added array with quote objects 
 let quotes = [
   {
     quote: 'If you try and take a cat apart to see how it works, the first thing you have on your hands is a nonworking cat.',
@@ -53,32 +45,19 @@ let quotes = [
   }
 ];
 
+/*
+   Created the getRandomQuote function to generate a random number.
+   Used the random number to return a random array index.
+*/
 
-
-/***
-  Create the `getRandomQuote` function to:
-   - generate a random number 
-   - use the random number to `return` a random quote object from the 
-     `quotes` array.
-***/
-// Created the getRandomQuote function to generate a random #/index
 
 function getRandomQuote(array) {
   i = Math.floor(Math.random() * 5);
   return array[i];
 }
 
-
-/***
-  Create the `printQuote` function to: 
-   - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to 
-     create your HTML string.
-   - use conditionals to make sure the optional properties exist before 
-     they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
+//Created the randomColor function to return a random rgb value.
+  
 function randomColor() {
   var x = Math.floor(Math.random() * 256);
   var y = Math.floor(Math.random() * 256);
@@ -86,7 +65,13 @@ function randomColor() {
   var rgbColor = 'rgb(' + x + ',' + y + ',' + z + ')';
   document.body.style.background = rgbColor;
 }
-// Created the printQuote function to print a random quote to the screen
+
+/*
+  Created the printQuote function to print a random quote to the screen.
+  Added conditional logic statements for the extra properties.
+  Added the randomColor function call to change colors with the quotes.
+  Added the message string to the page with the provided code.
+*/   
 function printQuote() {
   let randomQuote = getRandomQuote(quotes);
   let message = '';
@@ -104,17 +89,12 @@ function printQuote() {
   randomColor();
   document.getElementById('quote-box').innerHTML = message;
 }
+// Used the setInterval method to call the printQuote function every 10 seconds.
 
 setInterval(function(){printQuote()}, 10000);
 
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
+// Code that calls printQuote function on button click.
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
